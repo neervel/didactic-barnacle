347dd8798bf2de91f42f31edb284a2f84fc44b9d
Kirill Likhopoy <kirill.likhopoy@1win.pro>
2023-01-20 00:23:56 +0600

    Test task

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello, world!');
+const openMenu = document.querySelector('.js-menu-open')
+const closeMenu = document.querySelector('.js-menu-close')
+const menu = document.querySelector('.js-menu-drodpown')
+
+const toggleDropdown = () => {
+    menu.classList.toggle('show')
+    openMenu.classList.toggle('hide')
+    closeMenu.classList.toggle('hide')
+}
+
+openMenu.addEventListener('click', () => {
+    toggleDropdown()
+})
+
+closeMenu.addEventListener('click', () => {
+    toggleDropdown()
+})
