0947080a5bf58a5ca2f6b88877256a88b463f061
Kirill Likhopoy <kirill.likhopoy@1win.pro>
2023-01-20 12:11:41 +0600

    Add animated button

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const openMenu = document.querySelector('.js-menu-open')
-const closeMenu = document.querySelector('.js-menu-close')
+const toggleMenu = document.querySelector('.js-menu-toggle')
 const menu = document.querySelector('.js-menu-drodpown')
 
 const toggleDropdown = () => {
     menu.classList.toggle('show')
-    openMenu.classList.toggle('hide')
-    closeMenu.classList.toggle('hide')
+    toggleMenu.classList.toggle('close')
 }
 
-openMenu.addEventListener('click', () => {
-    toggleDropdown()
-})
-
-closeMenu.addEventListener('click', () => {
+toggleMenu.addEventListener('click', () => {
     toggleDropdown()
 })
