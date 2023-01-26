b5318467f3ff5311c1c10d513f86967ccb923548
Kirill Likhopoy <kirill.likhopoy@1win.pro>
2023-01-27 00:25:04 +0600

    Make slider smoother & better

--- index.js
+++ index.js
@@ -17,7 +17,51 @@ createApp(
     {
         data() {
             return {
-                sliderPosition: 1,
+                sliderPosition: 650,
+                lastRangePosition: 650,
+                currentPosition: 1,
+                maxRange: 1300,
+                midRange: 650,
+            }
+        },
+
+        methods: {
+            handleRangeChange(e) {
+                const newVal = e.target.value
+
+                if (newVal - this.lastRangePosition >= 0) {
+                    const rangeTarget = this.currentPosition === 0 && newVal <= this.midRange ? this.midRange : this.maxRange
+                    while(this.sliderPosition < rangeTarget) {
+                        this.sliderPosition++
+                    }
+                    if (this.currentPosition === 0 && newVal > this.midRange) this.currentPosition += 2
+                    else this.currentPosition++
+                    this.lastRangePosition = rangeTarget
+                } else if ((newVal - this.lastRangePosition < 0)) {
+                    const rangeTarget = this.currentPosition === 2 && newVal > this.midRange ? this.midRange : 0
+                    while(this.sliderPosition > rangeTarget) {
+                        this.sliderPosition--
+                    }
+                    if (this.currentPosition === 2 && newVal <= this.midRange) this.currentPosition -= 2
+                    else this.currentPosition--
+                    this.lastRangePosition = rangeTarget
+                }
+            },
+
+            handleSlideClick(side) {
+                if (side === 'left' && this.currentPosition !== 2) {
+                    while(this.sliderPosition < this.maxRange) {
+                        this.sliderPosition++
+                    }
+                    this.currentPosition++
+                    this.lastRangePosition = this.maxRange
+                } else if (side === 'right' && this.currentPosition !== 0) {
+                    while(this.sliderPosition > 0) {
+                        this.sliderPosition--
+                    }
+                    this.currentPosition--
+                    this.lastRangePosition = 0
+                }
             }
         }
     }
